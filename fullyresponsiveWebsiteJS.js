cddd3f735cd5bf3c5b019379bf4491ff05d707c5
A1Kumari <94031882+A1Kumari@users.noreply.github.com>
2022-05-11 20:55:26 +0530

    Add files via upload

---
+++ fullyresponsiveWebsiteJS.js
@@ -0,0 +1,8 @@
+burger = document.querySelector(".burger")
+navbar = document.querySelector(".navbar")
+rightNav = document.querySelector(".rightNav")
+
+burger.addEventListener('click',()=>{
+    rightnav.classList.toggle('v-class-resp')
+    navList.classList.toggle('v-class-resp')
+})
